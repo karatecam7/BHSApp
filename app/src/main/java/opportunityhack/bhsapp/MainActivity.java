440d2cd6446b00343ff0edef98ba76e1414bb657
BuildTools <unconfigured@null.spigotmc.org>
2017-02-15 10:43:06 -0500

    made a button and an action bar

--- app/src/main/java/opportunityhack/bhsapp/MainActivity.java
+++ app/src/main/java/opportunityhack/bhsapp/MainActivity.java
@@ -1,7 +1,9 @@
 package opportunityhack.bhsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void goToActivity2(View view) {
+        Intent intent = new Intent(this, InfoPage1.class);
+        startActivity(intent);
+    }
+
 }
