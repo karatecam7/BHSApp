f154c30907e0e935bda8a1f7f9595ac93215df7e
karatecam7 <karatecam7@gmail.com>
2017-03-29 10:36:20 -0400

    changed the pages again. I may have fixed the error, but I can't really tell right now. :/

--- app/src/main/java/opportunityhack/bhsapp/CatSepAnx.java
+++ app/src/main/java/opportunityhack/bhsapp/CatSepAnx.java
@@ -3,7 +3,6 @@ package opportunityhack.bhsapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class CatSepAnx extends AppCompatActivity {
