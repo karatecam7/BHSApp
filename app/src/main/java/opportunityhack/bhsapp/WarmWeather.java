3d4ba72c6773f029f055088e5e38239703d0482a
Jeff Hammond <jefhammond@paypal.com>
2017-03-31 10:13:48 -0400

    Fixing Activity Bug linking to Activity classes

--- app/src/main/java/opportunityhack/bhsapp/WarmWeather.java
+++ app/src/main/java/opportunityhack/bhsapp/WarmWeather.java
@@ -10,64 +10,12 @@ public class WarmWeather extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.change_dog_name);
     }
 
-    public void goToFirstAid(View view) {
-        Intent intent = new Intent(this, FirstAid.class);
+    public void goToActivitymain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-    }
-
-    public void goToFoodToAvoid(View view) {
-        Intent intent = new Intent(this, FoodToAvoid.class);
-        startActivity(intent);
-    }
-
-    public void goToFourth(View view) {
-        Intent intent = new Intent(this, FourthOfJuly.class);
-        startActivity(intent);
-    }
-
-    public void goToSidewalks(View view) {
-        Intent intent = new Intent(this, HotSidewalk.class);
-        startActivity(intent);
-    }
-
-    public void goToHouseHazards(View view) {
-        Intent intent = new Intent(this, HouseHazard.class);
-        startActivity(intent);
-    }
-
-    public void goToSwim(View view) {
-        Intent intent = new Intent(this, Swim.class);
-        startActivity(intent);
-    }
-
-    public void goToLitter(View view) {
-        Intent intent = new Intent(this, LitterBox.class);
-        startActivity(intent);
-    }
-
-    public void goToLost(View view) {
-        Intent intent = new Intent(this, LostPet.class);
-        startActivity(intent);
-    }
-
-    public void goToColdWeather(View view) {
-        Intent intent = new Intent(this, ColdWeather.class);
-        startActivity(intent);
-    }
-
-    public void goToChangeName(View view) {
-        Intent intent = new Intent(this, ChangeDogName.class);
-        startActivity(intent);
-    }
-
-    public void goToCatSeperation(View view) {
-        Intent intent = new Intent(this, CatSepAnx.class);
-        startActivity(intent);
-
     }
 
 }
